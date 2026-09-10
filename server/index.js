b23dae5e70d8c8ae87c0459d2716669c0b21328b
Madhumitha R <98893549+madhumitha-r26@users.noreply.github.com>
2026-09-10 08:30:28 +0000

    final deploy-5

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ DbConnection();
 app.use(express.json());
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: [
+    "https://blood-hive-blood-bank.vercel.app",
+    "http://localhost:5173"
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
